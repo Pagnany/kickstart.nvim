efdd71ae952343c50902a50e78ada9743a82c35c
Eric Schmale <eric.schmale@gmx.de>
2024-05-18 09:32:36 +0200

    elixir-lsp

--- init.lua
+++ init.lua
@@ -576,6 +576,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
         tsserver = {},
+        elixirls = {},
 
         lua_ls = {
           -- cmd = {...},
